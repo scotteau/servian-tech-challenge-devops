a85eff4378f9197831075943d6533c21cf28a09b
scottw9812 <dev@scottw.xyz>
2022-12-05 21:34:52 +0800

    added alb & iam

---
+++ 04-alb.tf
@@ -0,0 +1,43 @@
+#################################################################################
+## alb
+#################################################################################
+resource "aws_alb_target_group" "ecs" {
+  name        = "${local.prefix}-target-group"
+  port        = 80
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
+  target_type = "ip"
+
+  health_check {
+    path                = "/healthcheck"
+    port                = "traffic-port"
+    healthy_threshold   = 5
+    unhealthy_threshold = 2
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
+  }
+
+  tags = local.default_tags
+}
+
+resource "aws_lb" "alb" {
+  name               = "${local.prefix}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = tolist(aws_subnet.public[*].id)
+
+  tags = local.default_tags
+}
+
+resource "aws_lb_listener" "redirect-to-https" {
+  load_balancer_arn = aws_lb.alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_alb_target_group.ecs.arn
+  }
+}
---
+++ 05-iam.tf
@@ -0,0 +1,47 @@
+data "aws_iam_policy_document" "ecs_tasks_execution_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["ecs-tasks.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "ecs_task_execution_role" {
+  name               = "ecs_task_execution_role"
+  description        = "IAM role for ecs task execution role"
+  assume_role_policy = data.aws_iam_policy_document.ecs_tasks_execution_role.json
+
+  managed_policy_arns = [
+    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy",
+    aws_iam_policy.ssm_access.arn
+  ]
+
+  tags = local.default_tags
+}
+
+resource "aws_iam_policy" "ssm_access" {
+  name   = "ecs_ssm_access"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "ssm:GetParameters"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+
+
+
